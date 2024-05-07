a739ea99bd3aa260ea6e5aee03bb03c8087c2b54
Jenkins Course <jenkins@theconstruct.ai>
2024-05-07 07:39:43 +0000

    changes

--- scripts/test_move_and_turn_service.py
+++ scripts/test_move_and_turn_service.py
@@ -27,6 +27,7 @@ if __name__ == '__main__':
     rospy.init_node('test_move_and_turn_service')
     print("1")
     rospy.wait_for_service('/start_move_and_turn')
+    rospy.loginfo("Started move_and_turn node")
     print("2")
     #test = TestMoveAndTurnService()
 
